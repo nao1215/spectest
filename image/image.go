b2d54e0989fc6c0c07e333f09bddb18e9ec32bb0
CHIKAMATSU Naohiro <naohiro.chikamatsu@fuller.co.jp>
2023-11-01 15:37:51 +0900

    Add EqualFromFileWithThreshold()

--- image/image.go
+++ image/image.go
@@ -14,36 +14,49 @@ import (
 // EqualFromFile verifies that the image file in expect is the same as the image in the response body.
 func EqualFromFile(expected string) func(*http.Response, *http.Request) error {
 	return func(res *http.Response, req *http.Request) error {
-		want, err := imaging.Open(expected)
-		if err != nil {
-			return err
-		}
-
-		tempFile, err := os.CreateTemp("", "image")
-		if err != nil {
-			return err
-		}
-		defer tempFile.Close() //nolint:errcheck
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close() //nolint:errcheck
-
-		if _, err = tempFile.Write(body); err != nil {
-			return err
-		}
-
-		got, err := imaging.Open(tempFile.Name())
-		if err != nil {
-			return err
-		}
-
-		result := imgdiff.Diff(want, got, &imgdiff.Options{Threshold: 0.1})
-		if !result.Equal {
-			return fmt.Errorf("image diff pixels count=%d", result.DiffPixelsCount)
-		}
-		return nil
+		return cmpImages(expected, 0.1, res)
 	}
+}
+
+// EqualFromFileWithThreshold verifies that the image file in expect is the same as the image in the response body.
+// The threshold is the maximum difference between the images. The value is between 0 and 1. Less more precise.
+func EqualFromFileWithThreshold(expected string, threshold float64) func(*http.Response, *http.Request) error {
+	return func(res *http.Response, req *http.Request) error {
+		return cmpImages(expected, threshold, res)
+	}
+}
+
+// cmpImages compares the image in the response body with the image in the expect file.
+func cmpImages(expected string, threshold float64, res *http.Response) error {
+	want, err := imaging.Open(expected)
+	if err != nil {
+		return err
+	}
+
+	tempFile, err := os.CreateTemp("", "image")
+	if err != nil {
+		return err
+	}
+	defer tempFile.Close() //nolint:errcheck
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close() //nolint:errcheck
+
+	if _, err = tempFile.Write(body); err != nil {
+		return err
+	}
+
+	got, err := imaging.Open(tempFile.Name())
+	if err != nil {
+		return err
+	}
+
+	result := imgdiff.Diff(want, got, &imgdiff.Options{Threshold: threshold})
+	if !result.Equal {
+		return fmt.Errorf("image diff pixels count=%d", result.DiffPixelsCount)
+	}
+	return nil
 }
